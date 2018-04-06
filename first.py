3d1ae9aef305ca6fb2c32a5e10da3b15a64928c6
Thiago Haigerti Bertoldi <thb96@hotmail.com>
2018-04-06 23:45:47 +0000

    Changed order of complexity of search O(n) -> O(log n)

--- first.py
+++ first.py
@@ -1,3 +1,12 @@
+from bisect import bisect_left
+
+def first_index(ordered_list, n):
+    'Locate the leftmost value exactly equal to n'
+    i = bisect_left(ordered_list, n)
+    if i != len(ordered_list) and ordered_list[i] == n:
+        return i
+    raise ValueError
+
 ordered_list = list(range(300))
 
 try:
@@ -5,8 +14,9 @@ try:
 except ValueError:
     print("Couldn't parse input, only integers allowed.")
     quit()
+
 try:
-    answer = ordered_list.index(num)
+    answer = first_index(ordered_list, num)
     while ordered_list[answer]==num:
         print("Found {} in position {}".format(num,answer))
         answer+=1
