d7232e82744f6a4f3756d9a80203ea3cd60d17e4
Thiago Bertoldi <thb96@hotmail.com>
2018-04-06 20:38:50 -0300

    Corrected error with accents.

--- fourth.py
+++ fourth.py
@@ -16,17 +16,19 @@ sentence = input("Waiting for words: ")
 words = sentence.split()
 for word in words:
     word_sum = 0
-    if word.isalpha():
-        for letter in word:
-            value = ord(letter)
-            if value > 96:
-                word_sum += value - 96
-            else:
-                word_sum += value - 38
+    valid = True
+    for letter in word:
+        value = ord(letter)
+        if (value > 96) and (value < 122):
+            word_sum += value - 96
+        elif (value > 64) and (value < 90):
+            word_sum += value - 38
+        else:
+            print("Invalid word, skipping")
+            valid = False
+            break
+    if valid:
         if is_prime(word_sum):
             print("Word {} is prime, value {}".format(word,word_sum))
         else:
             print("Word {} is not prime, value {}".format(word,word_sum))
-    else:
-        print("Invalid word, skipping")
-            
